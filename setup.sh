200b255d241f7ea58088e2c350129a51467ffbff
httpPrincess <ryba271+git@google.com>
2015-03-06 13:38:34 +0100

    docker compose added

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # setup
-DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y docker.io git python-virtualenv
-TEST_DIR=$TEST_DIR
+DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y docker.io git python-virtualenv libyaml-dev
+TEST_DIR=/testdir/
 mkdir $TEST_DIR
 cd $TEST_DIR
 virtualenv env
@@ -10,24 +10,22 @@ source env/bin/activate
 pip install -U git+https://github.com/httpPrincess/compose.git
 
 # make facade image
-mkdir -p $TEST_DIRfacade
-cd $TEST_DIRfacade 
 git clone https://github.com/httpPrincess/metahosting.git
-docker build --rm=true --no-cache=true -t facade . >> log.txt
-cd ..
+cd metahosting
+docker build --rm=true --no-cache=true -t facade . >> /tmp/log.txt
 
 # get worker
-mkdir -p $TEST_DIRworker
-cd $TEST_DIRworker/
+cd $TEST_DIR
 git clone https://github.com/BeneDicere/metahosting-worker.git
+cd metahosting-worker
 # we mix all in one environment
 pip install -r requirements.txt
-cd ..
 
 # integration starts
-mkdir -p $TEST_DIRintegration
-cd $TEST_DIRintegration
+cd $TEST_DIR
 git clone https://github.com/httpPrincess/meta-integration.git
+cd meta-integration
+export COMPOSE_CLIENT_VERSION=1.12
 docker-compose pull
 docker-compose start messaging db autho messaging updater
-docker-compose ps >> log.txt
+docker-compose ps >> /tmp/log.txt
