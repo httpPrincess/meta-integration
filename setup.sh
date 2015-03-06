f4fc70c0b62847f3945bb1bf10a10731c200e290
httpPrincess <ryba271+git@google.com>
2015-03-06 13:09:06 +0100

    vver

--- setup.sh
+++ setup.sh
@@ -2,10 +2,32 @@
 
 # setup
 DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y docker.io git python-virtualenv
-mkdir /testingdir/
-cd /testingdir/
+TEST_DIR=$TEST_DIR
+mkdir $TEST_DIR
+cd $TEST_DIR
 virtualenv env
 source env/bin/activate
 pip install -U git+https://github.com/httpPrincess/compose.git
-git checkout 
 
+# make facade image
+mkdir -p $TEST_DIRfacade
+cd $TEST_DIRfacade 
+git clone https://github.com/httpPrincess/metahosting.git
+docker build --rm=true --no-cache=true -t facade . >> log.txt
+cd ..
+
+# get worker
+mkdir -p $TEST_DIRworker
+cd $TEST_DIRworker/
+git clone https://github.com/BeneDicere/metahosting-worker.git
+# we mix all in one environment
+pip install -r requirements.txt
+cd ..
+
+# integration starts
+mkdir -p $TEST_DIRintegration
+cd $TEST_DIRintegration
+git clone https://github.com/httpPrincess/meta-integration.git
+docker-compose pull
+docker-compose start messaging db autho messaging updater
+docker-compose ps >> log.txt
