c6d4bb3b74947a5e555b6ef9c693fa5612a4715e
httpPrincess <ryba271+git@google.com>
2015-03-09 13:14:52 +0100

    cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# cleanup
+deactivate
+TEST_DIR=/testdir/
+rm -rf $TEST_DIR
+echo 'Killing instances'
+for i in `docker ps --all | awk '{print $1}'` ; do docker kill $i; docker rm $i; done
+
+echo 'Removing images'
+for i in `docker images | awk '{print $3}' |` ; do docker rmi $i; done
+
